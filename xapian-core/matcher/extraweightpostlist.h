998760d04560ba7b831e319604d9218af1ba9bf2
Gaurav Arora <gauravarora.daiict@gmail.com>
2014-03-08 19:07:44 +0000

    Remove passing unique term to get_sumextra since no weighting scheme is using it

--- xapian-core/matcher/extraweightpostlist.h
+++ xapian-core/matcher/extraweightpostlist.h
@@ -52,8 +52,11 @@ class ExtraWeightPostList : public PostList {
 	Xapian::docid  get_docid() const { return pl->get_docid(); }
 
 	double get_weight() const {
-	    return pl->get_weight() + wt->get_sumextra(pl->get_doclength(),
-						       pl->get_unique_terms());
+            /* Second parameter of get_sumextra is number of unique terms in doc, which has been put
+             * to maintain consistency with get_sumpart, As of now none of weighting scheme is using
+             * it. Current 0 is being passed, change it to pl->get_unique_terms() in case you
+             * need access uniq_terms. */
+	    return pl->get_weight() + wt->get_sumextra(pl->get_doclength(),0);
 	}
 
 	double get_maxweight() const {
